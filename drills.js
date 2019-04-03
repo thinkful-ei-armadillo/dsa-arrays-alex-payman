883b5a8c92189ee680e29f04d1ebc31271988d63
AReach <DerAReich@gmail.com>
2019-04-03 15:41:56 -0500

    completed drills

--- drills.js
+++ drills.js
@@ -28,6 +28,27 @@ function main() {
   //drill 5
 }
 
+// main();
+
+// the length is 6, the capacity is 12, and the memory address is 3.
+// length is 6 because we pushed 6 values into the array.
+// capacity is twelve because it went over the previous capacity, so we multiplied it and allocated the new space in memory.
+// memory address is 3 because we had to resize the array to accomodate the new values, which starts a new block and copies the values over.
+
+//3. Exploring the pop() method
+
+// the length is 3. This is because we pushed 6 values and then removed 3. 6 - 3 = 3.
+// the capacity is 12, this is because we had pushed 6 values. When we pop, we don't decrease the capacity, just the length.
+// memory address is 3 because we had to resize the array to accommodate new values. Poping doesn't change the pointer.
+
+//4. Understanding arrays
+
+//it return NaN because of how we initialize the memory in our memory file, it only takes Floats "Float64Array"
+//the purpose of the resize function in our array class is to reallocate memory whenever length exceeds the capactiy of our array
+
+//5. urlify
+
+
 //drill 5 , big o(n)
 function replaceSpace(str) {
   let newStr = '';
@@ -42,7 +63,7 @@ function replaceSpace(str) {
   return newStr;
 }
 
-console.log(replaceSpace('tauhida parveen'));
+// console.log(replaceSpace('tauhida parveen'));
 
 //drill 6
 function filter(arr) {
@@ -54,7 +75,7 @@ function filter(arr) {
   }
   return newArr;
 }
-console.log(filter([0, 1, 8, 9, 10, 12, 6, 4]));
+// console.log(filter([0, 1, 8, 9, 10, 12, 6, 4]));
 
 //drill 7
 
@@ -72,7 +93,7 @@ function maxSum(arr) {
   }
   return maxSum;
 }
-console.log(maxSum([4, 6, -3, 5, -30, 8, 12]));
+// console.log(maxSum([4, 6, -3, 5, -30, 8, 12]));
 
 //drill 8 bigO(n);
 
@@ -92,27 +113,79 @@ function mergeArr(arr1, arr2) {
   }
   return newArr;
 }
-console.log(mergeArr([1, 3, 6, 8, 11, 13], [2, 3, 5, 8, 9, 10, 12]));
+// console.log(mergeArr([1, 3, 6, 8, 11, 13], [2, 3, 5, 8, 9, 10, 12]));
 
 /*Input:[1, 3, 6, 8, 11] and [2, 3, 5, 8, 9, 10]
 Output:[1, 2, 3, 3, 5, 6, 8, 8, 9, 10, 11]*/
 
-main();
+function removeCharacters(str, filter) {
+  let newStr = '';
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (!filter.includes(char)) {
+      newStr += char;
+    }
+  }
+  return newStr;
+}
 
-// the length is 6, the capacity is 12, and the memory address is 3.
-// length is 6 because we pushed 6 values into the array.
-// capacity is twelve because it went over the previous capacity, so we multiplied it and allocated the new space in memory.
-// memory address is 3 because we had to resize the array to accomodate the new values, which starts a new block and copies the values over.
+// console.log(removeCharacters('Battle of the Vowels: Hawaii vs. Grozny', 'aeiou'));
 
-//3. Exploring the pop() method
+function products(arr) {
+  let result = 1;
+  for (let i = 0; i < arr.length; i++) {
+    result *= arr[i];
+  }
+  return arr.map(n => {
+    return result/n;
+  });
+}
 
-// the length is 3. This is because we pushed 6 values and then removed 3. 6 - 3 = 3.
-// the capacity is 12, this is because we had pushed 6 values. When we pop, we don't decrease the capacity, just the length.
-// memory address is 3 because we had to resize the array to accommodate new values. Poping doesn't change the pointer.
+// console.log(products([1,3,9,4]));
 
-//4. Understanding arrays
+function map2dArray(arr) {
+  let mappedArr = [];
 
-//it return NaN because of how we initialize the memory in our memory file, it only takes Floats "Float64Array"
-//the purpose of the resize function in our array class is to reallocate memory whenever length exceeds the capactiy of our array
+  let changeCol = [];
+  //for each row
+  for (let row = 0; row < arr.length; row++) {
+    //we're going to first copy it from the original
+    mappedArr.push(arr[row]);
+    //for each value in each row
+    for (let i = 0; i < arr[row].length; i++) {
+      //if the value === 0
+      if (arr[row][i] === 0) {
+        //if not in changedCol (indicates that col should be set to 0) add it
+        if (changeCol.indexOf(i) === -1) {
+          changeCol.push(i);
+        }
+        //map the whole row to 0
+        mappedArr[row] = mappedArr[row].map(() => 0);
+      }
+    }
+  }
+  //go over each row, for each marked column, set value to 0.
+  for (let row = 0; row < arr.length; row++) {
+    //for each row, change all the columns in changeCol to 0
+    for (let val of changeCol) {
+      arr[row][val] = 0;
+    }
+  }
+  return mappedArr;
+}
 
-//5. urlify
+function stringRotation(str1, str2) {
+  for (let i = 0; i < str2.length; i++) {
+    if (str2[i] === str1[0]) {
+      const lastPart = str2.slice(i);
+      const firstPart = str2.slice(0, i);
+      if (lastPart+firstPart === str1) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+// console.log(stringRotation('amazon','azonma'));
+// console.log(stringRotation('amazon','azonam'));
